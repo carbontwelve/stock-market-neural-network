4126eb19ae592d2d061997ad0b7d01e6648d8b44
Simon Dann <simon.dann@halesway.co.uk>
2016-06-27 14:51:58 +0100

    Added some comments for me so I know what I am doing

--- src/Commands/NeuralNetworkTrainCommand.php
+++ src/Commands/NeuralNetworkTrainCommand.php
@@ -16,6 +16,24 @@ class NeuralNetworkTrainCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        // To train the neural network you need to have at least 120 ticks worth of input data with a tick being the minimum
+        // resolution that you are dealing with (e.g if you run the scrape command every minute then you have one tick per minute)
+        //
+        // The reason why this needs at least 120 ticks worth of input data is because it feeds in the first 60 ticks and tests
+        // the prediction against the next 60 ticks one tick at a time.
+        //
+        // So if you have a weeks worth of data stored at a one minute tick resolution the training will create a window of
+        // 60 ticks to train the neural network and move the window along one tick, train and repeat until it gets to the most
+        // current input tick.
+        //
+        // The output vector is equal to fifteen ticks in the "future" and essentially just shows a possible trend.
+        // This method of input without doing any other manipulation of the input deltas is quite dumb but might provide
+        // interesting insight that would otherwise not be visible in the input data.
+
+        // [<--       Training Frame       -->][<-- Prediction Frame -->]
+        // [ 1, -1, -1, 1, -1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, 1, 1, 1, -1 ]
+        // As you train the network on the training frame you can use the known items covered by the prediction frame
+        // to test its output. Then move the whole stack to the left by one and repeat until you reach a point where the
+        // prediction frame hits the end of the available input data.
     }
 }
